05ae0f6dbaa59f98787ce60faed1d6c1c1431915
Andre <arc.xoc@gmail.com>
2022-11-22 08:12:04 -0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
+
+module.exports = nextConfig;
