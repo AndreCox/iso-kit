a366bc7fe18a4f42398555fcc5e1ba4667d70d3e
Andre <arc.xoc@gmail.com>
2022-11-22 22:07:08 -0800

    Init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
+import { Button, Navbar } from "@material-tailwind/react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { listen } from "@tauri-apps/api/event";
+import { useState } from "react";
 
 export default function Home() {
+  const onDrop = (e) => {
+    e.preventDefault();
+    console.log("File dropped");
+  };
+
+  listen("tauri://file-drop", (event) => {
+    console.log(event);
+  });
+
+  const [drag, setDrag] = useState(false);
+
   return (
     <div className="p-[0 2rem]">
       <Head>
@@ -12,65 +26,55 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen p-4 flex flex-1 flex-col justify-center items-center text-4xl">
-        <h1 className="font-bold text-center">
-          Welcome to{" "}
-          <a
-            href="https://github.com/andrecox/next-template"
-            className="text-blue-500 underline decoration-transparent hover:decoration-blue-500 transition-all"
-          >
-            Next Template!
-          </a>
-        </h1>
-        <h2 className="mt-2">
-          Powered by{" "}
-          <a
-            href="https://nextjs.org"
-            className="text-blue-500 underline decoration-transparent hover:decoration-blue-500 transition-all"
-          >
-            Next.js
-          </a>
-        </h2>
-
-        <p className="text-center m-16 text-[1.5rem]">
-          Get started by editing{" "}
-          <code className="bg-[#fafafa] rounded-md p-[0.75rem] font-mono text-[1.1rem]">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex items-center justify-center flex-wrap max-w-[800px]">
-          <a href="https://tailwindcss.com" className={styles.card}>
-            <h2>Tailwind CSS &rarr;</h2>
-            <p>
-              Learn about Tailwind keeps the app size small while still being
-              easy to use.
-            </p>
-          </a>
-
-          <a href="https://mobx.js.org/README.html" className={styles.card}>
-            <h2>MobX &rarr;</h2>
-            <p>A powerful way to manage state without tons of boilerplate!</p>
-          </a>
-
-          <a href="https://pages.github.com/" className={styles.card}>
-            <h2>Github Pages &rarr;</h2>
-            <p>Build a static site and deploy it using Github Actions.</p>
-          </a>
-
-          <a href="https://capacitorjs.com" className={styles.card}>
-            <h2>Capacitor JS &rarr;</h2>
-            <p>Build your web app for mobile with access to native features.</p>
-          </a>
+      <main className="min-h-screen flex flex-1 flex-col text-4xl">
+        <div className="h-14">
+          <div className="w-full fixed top-0 px-2">
+            <Navbar className="space-x-2 flex px-2 py-2">
+              <Button className="">Import ISOs</Button>
+              <div className="flex justify-end flex-grow h-fit space-x-2">
+                <Button>ISOs to GoD</Button>
+                <Button>Extract ISOs</Button>
+              </div>
+            </Navbar>
+          </div>
         </div>
-        <p className="text-center mt-16 text-[1.5rem]">
-          MobX state management built in!
-        </p>
-        <Link href="/mobx">
-          <a className="text-blue-500 under text-xl underline decoration-transparent  hover:decoration-blue-500 transition-all">
-            Check it out here
-          </a>
-        </Link>
+        {/* grid that splits screen vertically into two columns */}
+        <div className="flex flex-1 flex-col md:flex-row ">
+          {/* left column */}
+          <div
+            className={
+              "flex shadow-blue-500 flex-1 mx-2 mb-2 pt-2 flex-col border-r mt-3 px-2 space-y-2  rounded-lg transition-shadow duration-500 " +
+              (drag ? " shadow-blue-500 shadow-all-inner" : "")
+            }
+            onDrop={(e) => {
+              setDrag(false);
+              e.preventDefault();
+            }}
+            onDragOver={(e) => {
+              setDrag(true);
+              e.preventDefault();
+            }}
+            onDragLeave={(e) => {
+              setDrag(false);
+              e.preventDefault();
+            }}
+          >
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              Drop ISOs here
+            </div>
+            <Button variant="text" className="flex flex-col">
+              Disk1
+            </Button>
+            <Button variant="text" className="flex flex-col">
+              Disk1
+            </Button>
+            <Button variant="text" className="flex flex-col">
+              Disk1
+            </Button>{" "}
+          </div>
+          {/* right column */}
+          <div className="flex flex-1 flex-col px-2 mt-2">test</div>
+        </div>
       </main>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "all-inner": "0 0 6px rgb(0 0 0 / 0.05) inset",
+      },
+    },
   },
   plugins: [],
-};
+});
